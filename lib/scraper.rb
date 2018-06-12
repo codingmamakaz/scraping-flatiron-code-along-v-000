260da5d6ad8ed1cbc999618fab1da5a75632ad72
Kazumi Karbowski <kazkazinus@gmail.com>
2018-06-12 01:37:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
 
     def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      binding.pry
     end
 
     def print_courses
@@ -19,5 +20,5 @@ class Scraper
       end
     end
   end
-
+Scraper.new.get_page
 end
